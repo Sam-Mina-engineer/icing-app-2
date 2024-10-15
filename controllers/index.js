1e03fd73ae94e9d9f9a86a74aaec671a5f06ac66
Sam Mina <samandmissy2010@gmail.com>
2024-10-14 22:43:08 -0400

    Added login route

--- controllers/index.js
+++ controllers/index.js
@@ -9,16 +9,19 @@ const apiRoutes = require('./api/api');
 
 router.use('/api', apiRoutes);
 
-// Add more routes here as needed (collab with Jessikah)
+// Add a GET route for /login to render the login page
+
+router.get('/login', (req, res) => {
+  res.render('login');  // Render the login.handlebars template
+});
+
 
 router.get('/', (req, res) => {
   res.render('homepage');  
 });
 
 router.use((req, res) => {
-    res.status(404).render('404');  
-  });
-
-// Export the router
+  res.status(404).render('404');  
+});
 
 module.exports = router;
