290d03dad2b448e5be097cee432964fd35f73bca
Sam Mina <samandmissy2010@gmail.com>
2024-10-14 21:31:39 -0400

    Import Sequelize

--- models/index.js
+++ models/index.js
@@ -1,17 +1,20 @@
+const Sequelize = require('sequelize');  // Import Sequelize
+const sequelize = require('../config/connection');  // Import the Sequelize instance
+
 const Employee = require('./employee');
 const Order = require('./order');
 
+// Define associations
 
 Employee.hasMany(Order, {
   foreignKey: 'employee_id',
   onDelete: 'CASCADE'
 });
 
-
 Order.belongsTo(Employee, {
   foreignKey: 'employee_id'
 });
 
-module.exports = { Employee, Order };
+// Export the models and the sequelize connection
 
-// Fixed to match Sequelize and updated the foreign key
+module.exports = { sequelize, Employee, Order };
