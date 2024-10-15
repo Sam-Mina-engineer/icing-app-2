5bab353935704f1cb8ad951d749d81a3c862e8d5
Sam Mina <samandmissy2010@gmail.com>
2024-10-14 22:26:08 -0400

    Expanded connection Sequelize to be able to depend on individual variables

--- config/connection.js
+++ config/connection.js
@@ -1,16 +1,38 @@
 const Sequelize = require('sequelize');
 require('dotenv').config();
 
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
-  dialect: 'postgres',
-  protocol: 'postgres',
-  logging: false, 
-  dialectOptions: {
-    ssl: {
-      require: true, // For Render
-      rejectUnauthorized: false 
+// Expanding to rely on individual variables. 
+
+let sequelize;
+
+if (process.env.DATABASE_URL) {
+  sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect: 'postgres',
+    protocol: 'postgres',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
     }
-  }
-});
+  });
+} else {
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+      host: process.env.DB_HOST,
+      dialect: 'postgres',
+      port: process.env.DB_PORT,
+      dialectOptions: {
+        ssl: {
+          require: true,
+          rejectUnauthorized: false
+        }
+      }
+    }
+  );
+}
 
 module.exports = sequelize;
